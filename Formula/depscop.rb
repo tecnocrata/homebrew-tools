438b70925c5a94accfd1082ce6bd5c81aa560314
Enrique Ortuno <enrique@ortuno.net>
2024-07-02 22:40:36 -0400

    release 0.1.47

--- Formula/depscop.rb
+++ Formula/depscop.rb
@@ -1,8 +1,8 @@
 class Depscop < Formula
     desc "Utility to analyze dependencies in your projects"
     homepage "https://github.com/tecnocrata/deps-cop"
-    url "https://github.com/tecnocrata/deps-cop/releases/download/0.1.46/depscop-macos-arm64-0.1.46.zip"
-    sha256 "383f9ce2b603b7b88dc293814af00ee299c2b59e99933a671949fd71d1e4c2e3"
+    url "https://github.com/tecnocrata/deps-cop/releases/download/0.1.47/depscop-macos-arm64-0.1.47.zip"
+    sha256 "66a1a1368823b89320fda7f25722b6f70ab9a49d82b388cc1ec5de0852f73089"
   
     def install
       bin.install "depscop"
